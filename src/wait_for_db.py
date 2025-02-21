a030dc963c92e4dba17137826f01b99a700b0727
Oleksandr Barabanov <imtence@icloud.com>
2025-02-21 20:05:12 +0200

    add function wait_for_db to wait_for_db.py

---
+++ src/wait_for_db.py
@@ -0,0 +1,33 @@
+import os
+
+import time
+
+import django
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "cinema_service.settings"
+)
+django.setup()
+
+
+def wait_for_db():
+    print("Waiting for database...")
+    db_conn = None
+
+    while not db_conn:
+        try:
+            db_conn = connections["default"]
+            db_conn.cursor()
+            print("Database is available!")
+            break
+        except OperationalError:
+            print("Database is not ready, wait 2 seconds...")
+            time.sleep(2)
+
+
+if __name__ == "__main__":
+    wait_for_db()
