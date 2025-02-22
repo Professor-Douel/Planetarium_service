d3a940b5bc859e3c06dd6a9f1b00ab801a05b6d6
Oleksandr Barabanov <imtence@icloud.com>
2025-02-23 00:00:15 +0200

    wrap method create by with transaction.atomic in class ReservationSerializer

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from planetarium.models import (
     AstronomyShow,
@@ -71,8 +72,9 @@ class ReservationSerializer(serializers.ModelSerializer):
         )
 
     def create(self, validated_data):
-        tickets_data = validated_data.pop("tickets")
-        reservation = Reservation.objects.create(**validated_data)
-        for ticket_data in tickets_data:
-            Ticket.objects.create(**ticket_data)
-        return reservation
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(**ticket_data)
+            return reservation
