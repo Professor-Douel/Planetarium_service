bc387a0c17fda64cea551a83615dfe9ebc8b40f9
Oleksandr Barabanov <imtence@icloud.com>
2025-02-22 20:40:17 +0200

    add data to Class Yser in user/models.py

--- user/models.py
+++ user/models.py
@@ -1,4 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 
+from django.db import models
+
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
